00507598199ec536a56d768e9ad337c173b5351f
bobo <mbdiallo@devalto.com>
2023-02-09 12:57:12 +0000

    IFEF-058: Show link of job's description in details job.

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -42,6 +42,7 @@ class UserType extends AbstractType {
                 'required' => false
             ])
             ->add('residenceCountry', EntityType::class, [
+				// 'required' => false,
                 'class' => Country::class,
                 'placeholder' => 'Sélectionnez votre pays de résidence',
                 'attr' => [
